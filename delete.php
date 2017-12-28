29b50988798150a954b0abb97d719655466a33ac
shekhar <shekhar.lapp@gmail.com>
2017-12-28 17:54:56 +0530

    initial commit

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_POST["id"]))
+{
+ $connect = new PDO('mysql:host=localhost;dbname=ajaxdb', 'root', 'sekhar');
+ $query = "
+ DELETE from events WHERE id=:id
+ ";
+ $statement = $connect->prepare($query);
+ $statement->execute(
+  array(
+   ':id' => $_POST['id']
+  )
+ );
+}
